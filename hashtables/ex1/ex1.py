2ab57bb0a36fbcaf56227495effdea8b642322f8
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-08 13:37:04 -0400

    solve ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,6 +1,23 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
 
-    return None
+    # dict of weights to indices:
+    weight_to_index_dict = {weight: index for (index, weight) in enumerate(weights)}
+
+    # set default output
+    index_tuple = (None, None)
+
+    # iterate through dict to find limit:
+    for weight_1 in weight_to_index_dict.keys():
+
+        weight_2 = limit - weight_1
+
+        if weight_2 in weight_to_index_dict:
+
+            index_tuple = (
+                weight_to_index_dict[weight_1],
+                weight_to_index_dict[weight_2],
+            )
+
+            break
+
+    return index_tuple
