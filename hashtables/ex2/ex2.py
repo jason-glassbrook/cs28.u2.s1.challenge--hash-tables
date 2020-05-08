db9beb5a2988f3b506c408029ff86093b4380ac9
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-08 13:48:53 -0400

    solve ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,18 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
+    # dict of sources to destinations:
+    source_to_destination_dict = {ticket.source: ticket.destination for ticket in tickets}
+
+    # set default output
+    route = []
+
+    # first ticket is...
+    if None in source_to_destination_dict:
+        route.append(source_to_destination_dict[None])
+
+    # iterate through remaining tickets...
+    while route[-1] is not None:
+        route.append(source_to_destination_dict[route[-1]])
+
     return route
