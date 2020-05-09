957e709803ffc40f0da233d7cedbaae4163a683b
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-08 20:34:41 -0400

    solve ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,9 +1,22 @@
-def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
+from collections import defaultdict as DefaultDict
+import os
 
-    return result
+
+def finder(paths, queries):
+
+    # map bases to paths
+    bases_to_paths = DefaultDict(list)
+    for p in paths:
+        b = os.path.basename(p)
+        bases_to_paths[b].append(p)
+
+    # find query paths
+    query_paths = []
+    for q in queries:
+        if q in bases_to_paths:
+            query_paths.extend(bases_to_paths[q])
+
+    return query_paths
 
 
 if __name__ == "__main__":
