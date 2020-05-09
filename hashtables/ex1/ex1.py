bd5bb56bde2b4d7ac87fd0a58e7247be87f49236
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-08 21:27:01 -0400

    re-solve ex1, handling duplicates

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,27 +1,40 @@
+from collections import defaultdict as DefaultDict
+
+
 def get_indices_of_item_weights(weights, length, limit):
 
-    # map weights to indices:
-    weight_to_index_map = {weight: index for (index, weight) in enumerate(weights)}
+    # map weights to indices, allowing duplicates:
+    weight_to_indices_map = DefaultDict(list)
+    for (i, w) in enumerate(weights):
+        weight_to_indices_map[w].append(i)
 
     # set default `matches`
-    matches = (None, None)
+    matches = None
 
     # find indices of weights which sum to limit:
-    for weight_1 in weight_to_index_map:
+    for (w1, i1s) in weight_to_indices_map.items():
 
-        weight_2 = limit - weight_1
+        # the indices of weight 1
+        i1s = weight_to_indices_map[w1]
 
-        if weight_2 in weight_to_index_map:
+        # the next necessary weight
+        w2 = limit - w1
 
-            matches = (
-                weight_to_index_map[weight_1],
-                weight_to_index_map[weight_2],
-            )
+        # the indices of weight 2, maybe...
+        i2s = weight_to_indices_map[w2] if w2 in weight_to_indices_map else None
 
-            break
+        # if there could be any, try to get matching indices
+        if i1s and i2s:
 
-    # override `matches` if none found
-    if None in matches:
-        matches = None
+            # if weights are different, use first index of both
+            if w1 != w2:
+                matches = (i1s[0], i2s[0])
+                break
+
+            # if weights are same, there need to be enough indices
+            # i1s == i2s, in this case
+            elif len(i1s) > 1:
+                matches = tuple(i1s[0:2])
+                break
 
     return matches
